55de37015b78dfe7eabc8b73420660aa5cac24ed
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-21 00:39:25 +0330

    create database pool

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host :'',
-    user : '',
-    database: '',
-    password : '',
+    host :process.env.DB_HOST,
+    user : process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password : process.env.DB_PASS,
 
 
 })

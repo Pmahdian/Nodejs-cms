dc174e48506cc19ebddbac0572afd6d2a773a80c
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-23 03:03:16 +0330

    validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,5 +4,11 @@ const pool = require('../config/database');
 const register = async (req,res) =>{
     const {username, email, password} = req.body;
 
+    //validation
+    if (!username || !email || !password)
+        return res.status(400).json({error : 'every fileds are required!'});
+
+    
+
 
 }

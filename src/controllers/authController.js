41c730ccc165c1e102ec51a2e43c29835e3590ed
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-23 03:13:29 +0330

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,20 @@ const register = async (req,res) =>{
     if (!username || !email || !password)
         return res.status(400).json({error : 'every fileds are required!'});
 
-    
+    pool.query('select * from users where email = ? or username = ?',
+        [email, username],
+        (error,result) => {
+            if (error) 
+                return res.status(500).json({error : 'there is an error with database'});
+
+        }
+
+
+
+
+    )
+
+
 
 
 }

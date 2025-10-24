c10d840602cc854e28ea2bc15ac8c708b8b619a1
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-25 00:57:29 +0330

    take data from req.body

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,11 +3,12 @@ const pool = require('../config/database');
 
 const register = async (req,res)=>{
     try {
+       const {username , email, password} = await req.body
 
 
 
 
-        
+
         
     } catch (error) {
         

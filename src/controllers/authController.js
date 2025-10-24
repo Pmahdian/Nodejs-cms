64c17006aada4a928a1e86dc060ff1c1eef5deae
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-25 01:01:03 +0330

    validation step

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,6 +6,12 @@ const register = async (req,res)=>{
         //step 1
        const {username , email, password} = await req.body;
 
+       //step 2 - validation
+       if (!username || !email || !password)
+            return res.status(400).json({error : 'All fields are required!'});
+
+       
+
 
 
 

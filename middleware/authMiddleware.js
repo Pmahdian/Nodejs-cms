f0141f281564e0161050b1e79d69a91e86f56f56
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-25 21:15:24 +0330

    step 1 : get token from req headers

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
     try {
+        //step 1 : get token from req headers
+        const token = req.headers.authorization;
         
     } catch (error) {
         

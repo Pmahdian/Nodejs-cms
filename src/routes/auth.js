4964b26a090902dfa5037d471b60781f07793c10
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-25 22:59:16 +0330

    update structue

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController'); 
+const { register, login } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
-const { testAuth } = require('../controllers/testController');
+const { testAuth } = require('../controllers/testController'); 
 
-router.post('/register', register); 
-router.post('/login',login)
+// Routes 
+router.post('/register', register);
+router.post('/login', login);
 
-router.get('/test-protected', authMiddleware, testAuth)
+
+router.get('/test-protected', authMiddleware, testAuth);
 
 module.exports = router;
-
-
-

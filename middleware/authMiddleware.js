db4a8380a928f4c5962c7bbb17ee79679f06365d
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-25 21:18:07 +0330

    step 2 : Checking the existence of the token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,6 +5,12 @@ const authMiddleware = (req, res, next) => {
     try {
         //step 1 : get token from req headers
         const token = req.headers.authorization;
+
+        //step 2 : Checking the existence of the token
+        if(!token)
+            return res.status(401).json({error : 'Token is not found!'})
+
+        
         
     } catch (error) {
         

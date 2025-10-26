07b9b06490480be68285b556278aae97355e6ac5
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-26 21:46:00 +0330

    import getAllPosts module to routes/posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 
-const {createPost} = require('../controllers/postController');
+const {createPost, getAllPosts} = require('../controllers/postController');
 
 
 // POST /api/posts + '/' = /api/posts/

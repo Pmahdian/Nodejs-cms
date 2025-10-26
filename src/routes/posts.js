64a34df5bb973d2ea4dba2dd5fd51fc07481e86b
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-26 12:52:09 +0330

    // POST /api/posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,6 +4,8 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 const {createPost} = require('../controllers/postController');
 
+
+// POST /api/posts
 router.post('/', authMiddleware, createPost);
 
 module.exports = router;

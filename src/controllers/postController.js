0465a3eaff2b3440b67e0b82133fd8028ce5102f
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-26 12:13:28 +0330

    step 3 : date validation

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -7,7 +7,16 @@ const createPost = async (req, res) => {
 
         //step 2 : get userId from req.user moddleware
         const userId = req.user.userId
-        
+
+        //step 3 : date validation
+        if (!title || !content)
+            return res.status(400).json({
+                success : false,
+                error : 'title and content fields are required!'
+            });
+
+        /
+
 
         
     } catch (error) {

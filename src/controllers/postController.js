14b99b9a895f2e5fe9407ccb2ed702e6b8cdba8b
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-26 12:03:25 +0330

    step 1 : get data from request body

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,9 @@
 const createPost = async (req, res) => {
     try {
+        //step 1 : get data from request body
+        const {title, content, category_id} = req.body;
+
+        
         
     } catch (error) {
         

4203ededea39b22a03cc7b9e30d48942c0b185fc
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-26 22:16:18 +0330

    step 3 : send response

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -53,11 +53,11 @@ const getAllPosts = async (req, res) => {
             'SELECT p.id, p.title, p.content, p.created_at, u.username as author, c.name as category_name FROM posts p LEFT JOIN users u ON p.user_id = u.id LEFT JOIN categories c ON p.category_id = c.id ORDER BY p.created_at DESC'
         );
 
-        // step 2 : send response to user
+        // step 2 : send response
         if (posts.length === 0)
             return res.status(200).json({
                 success : true,
-                message : "There is no posts", 
+                message : "There is no post", 
                 posts : []
         
             });
@@ -96,10 +96,29 @@ const getMyPosts = async (req, res) => {
         const userId = req.user.userId
 
         //step 2 : get posts of the user from database
-        const result = await pool.query('SELECT p.id, p.title, p.content, p.created_at, u.username as author, c.name as category_name FROM posts p LEFT JOIN users u ON p.user_id = u.id LEFT JOIN categories c ON p.category_id = c.id WHERE p.user_id = ? ORDER BY p.created_at DESC', [userId]);
+        const [posts] = await pool.query('SELECT p.id, p.title, p.content, p.created_at, u.username as author, c.name as category_name FROM posts p LEFT JOIN users u ON p.user_id = u.id LEFT JOIN categories c ON p.category_id = c.id WHERE p.user_id = ? ORDER BY p.created_at DESC', [userId]);
 
-        
+        //step 3 : send response
+        if (posts.length === 0) 
+            return res.status(200).json(
+        {
+            success : true,
+            message : "There is no post",
+            posts : []
+        });
+        res.status(200).json(
+            {
+                success : true,
+                message : "Posts of following user received successfully.",
+                count : posts.length,
+                posts : posts
+            }
+        )
+
+
     } catch (error) {
+        console.error('Get post of follwing user error:', error);
+        
         
     }
 }

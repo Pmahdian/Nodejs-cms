58a331805d5431f01420985031da9864bca98955
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-26 12:04:25 +0330

    import authMiddleware

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,9 +1,12 @@
+const authMiddleware = require('../middleware/authMiddleware');
+
 const createPost = async (req, res) => {
     try {
         //step 1 : get data from request body
         const {title, content, category_id} = req.body;
 
-        
+        //step 2 : get userId from req.user moddleware
+
         
     } catch (error) {
         

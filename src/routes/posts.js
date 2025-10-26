302b39d6eec6577fd2181010512a997c0630c5c2
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-26 22:26:36 +0330

    add getMyPosts route

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 
-const {createPost, getAllPosts} = require('../controllers/postController');
+const {createPost, getAllPosts, getMyPosts} = require('../controllers/postController');
 
 
 // POST /api/posts + '/' = /api/posts/
 router.post('/', authMiddleware, createPost);
-// Get /api/posts + '/' = /api/posts/
+// GET /api/posts + '/' = /api/posts/
 router.get('/', getAllPosts)
+// GET myPosts
+router.get('/my-posts', authMiddleware, getMyPosts);
+
 
 
 module.exports = router;

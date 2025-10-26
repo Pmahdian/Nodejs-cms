8041dfeaa5e78850a0d5529314de2d6da8275f25
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-27 01:07:19 +0330

    step 2 : get a post by postId from database

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -135,7 +135,23 @@ const getPostById = async (req, res) => {
         //step 1 : get post id from req.params
         const postId = req.params.id;
 
-        //step 2 : get a post with post_id
+        //step 2 : get a post by postId from database
+        const [posts] = await pool.query(
+            `SELECT 
+                p.id,
+                p.title, 
+                p.content,
+                p.created_at,
+                p.updated_at,
+                u.username as author,
+                c.name as category_name
+            FROM posts p
+            LEFT JOIN users u ON p.user_id = u.id
+            LEFT JOIN categories c ON p.category_id = c.id
+            WHERE p.id = ?`,
+            [postId]
+        );
+
         
     } catch (error) {
         

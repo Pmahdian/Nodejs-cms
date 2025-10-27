47b66a324ef2453f4c9f230cf3b631f1c9899ce0
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-27 23:18:19 +0330

    step 2 : find user in databse without password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,12 @@ const getProfile = async (req, res) => {
     try {
         //step 1 : get userId from req.user middleware
         userId = req.user.userId;
+
+        //step 2 : find user in databse without password
+        result = await pool.query(
+            'select username, email from users where id = ?',
+            [userId]
+        );
         
     } catch (error) {
         

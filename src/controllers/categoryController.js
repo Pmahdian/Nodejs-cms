05716964c079f90bcc35159112c7fe1c234d744b
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-27 20:38:51 +0330

    step 1 : get all categories from database

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -2,6 +2,12 @@ const pool = require('../config/database');
 
 const getAllCatgories = async (req, res) => {
     try {
+        //step 1 : get all categories from database
+        const [categories] = await pool.query(
+            'select * from categories order by name ASC'
+        );
+
+        
         
     } catch (error) {
         

9a685f96337e728c54c3c1dbd28619d30e91efc6
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-27 23:15:34 +0330

    step 1 : get userId from req.user middleware

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@ const pool = require('../config/database');
 
 const getProfile = async (req, res) => {
     try {
+        //step 1 : get userId from req.user middleware
+        userId = req.user.userId;
         
     } catch (error) {
         

6f41f3eebd72ada42ff7d46de0b81bfa26a74db9
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-27 23:27:11 +0330

    update step 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,13 +3,24 @@ const pool = require('../config/database');
 const getProfile = async (req, res) => {
     try {
         //step 1 : get userId from req.user middleware
-        userId = req.user.userId;
+        const userId = req.user.userId;
 
         //step 2 : find user in databse without password
-        result = await pool.query(
+        const result = await pool.query(
             'select username, email from users where id = ?',
             [userId]
         );
+
+    
+        if (result === 0){
+            return res.status(404).json(
+                {
+                    success : false,
+                    message : 'user not found'
+                }
+            )
+
+        }
         
     } catch (error) {
         

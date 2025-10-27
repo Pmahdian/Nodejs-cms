358050b4988c6a2add50d97e61e479566b02f422
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-27 21:28:00 +0330

    add routes to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const postRoutes = require('./src/routes/posts');
 const authRoutes = require('./src/routes/auth');
+const categoriesRoutes = require('./src/routes/categories');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 //Router
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/categories', categoriesRoutes);
 
 app.get('/', (req,res)=>{
     res.send('hello...')
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+//Controller
+const {getAllCategories, createCategory} = require('../controllers/categoryController')
+
+// router
+router.get('/',getAllCategories);
+router.post('/',createCategory);
+
+module.exports = router;

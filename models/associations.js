a6e09bfd718b869875643ac3526a7a186cf11179
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-30 23:07:36 +0330

    define associations

--- models/associations.js
+++ models/associations.js
@@ -1,3 +1,12 @@
 const User = require('./User');
 const Post = require('./Post');
 const Category = require('./Category');
+
+User.hasMany(Post, {foreignKey : 'user_id'});
+Post.belongsTo(User, {foreignKey : 'user_id'});
+
+
+Category.hasMany(Post, {foreignKey : 'user_id'});
+Post.belongsTo(Category, {foreignKey : 'user_id'});
+
+module.exports = { User, Post, Category };

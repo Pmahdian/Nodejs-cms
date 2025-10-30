ccbb825d5bc5c20b2b0bdee2cce1a4eb3e6edc53
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-30 13:52:01 +0330

    step 1 : connection test

--- test-model.js
+++ test-model.js
@@ -3,6 +3,11 @@ const User = require('./models/User');
 
 async function testModel() {
     try {
+        console.log('🔌 Connecting to database...');
+
+        // step 1 : connection test
+        await sequelize.authenticate();
+        console.log('✅ connected to the database!');
         
     } catch (error) {
         

fec72f13d10a5c620a9556c122f8daf7b06531e9
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-30 23:13:50 +0330

    change content datatype

--- models/Post.js
+++ models/Post.js
@@ -7,7 +7,7 @@ const Post = sequelize.define('Post',{
         allowNull : false
     },
     content : {
-        type : DataTypes.STRING,
+        type : DataTypes.TEXT,
         allowNull : false
     },
     user_id : {

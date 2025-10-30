53819e5a49d9ddc89e8d5d5867814901b9739d3b
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-30 22:33:25 +0330

    define post cilumn with sequelize

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,31 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/sequelize');
 
+const post = sequelize.define('Post',{
+    title : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    content : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    user_id : {
+        type : DataTypes.INTEGER,
+        allowNull : false,
+
+    },
+    category_id : {
+        type : DataTypes.INTEGER,
+        allowNull : true
+    }
+}, 
+    {
+        tableName : 'posts',
+        timestamps : true,
+        underscored : true,
+        createdAt : 'created_at',
+        updatedAt : 'updated_at'
+
+    }
+);

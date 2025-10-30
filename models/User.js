4d989ffe2f17f0501b2da3ecfcfaf398b76c8a65
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-30 12:58:33 +0330

    define username column

--- models/User.js
+++ models/User.js
@@ -3,5 +3,11 @@ const { sequelize } = require('../config/sequelize');
 
 
 const User = sequelize.define('User', {
+    // define username field
+    username : {
+        type : DataTeypes.STRING,
+        allowNull : false,
+        uniqie : true
+    },
     
 })

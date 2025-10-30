978a6e8aa5c7695e9fd0b5a16277621d3e5da75b
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-30 22:43:53 +0330

    define category

--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,21 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/sequelize');
 
+const Category = sequelize.define('category',{
+    name : {
+        type : DataTypes.STRING,
+        allowNull : false,
+        unique : true
+    },
+    description : {
+        type : DataTypes.TEXT,
+        allowNull : true
+    }
+},
+{
+    tableName : 'categories',
+    timestamps : true,
+    underscored : true,
+    createdAt : 'created_at',
+    updatedAt : false
+})

a5d2a9f289f80dadf338c12715e3435236908620
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-31 16:40:48 +0330

    exporting

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -29,4 +29,9 @@ const registerSchema = Joi.object(
     const loginSchema = Joi.object({
         email : Joi.string().email(). required(),
         password : Joi.string().required()
-    })
+    });
+
+    module.exports = {
+        registerSchema,
+        loginSchema
+    }

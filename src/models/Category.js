66394ab4b0cfd50f2a29695a0ed968f8b8e57162
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-31 14:10:25 +0330

    move model folder

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/sequelize');
+const { sequelize } = require('../../config/sequelize');
 
 const Category = sequelize.define('Category',{
     name : {

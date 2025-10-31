2fec986c6845b6615988afc85253a5ffb020d26a
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-10-31 16:30:35 +0330

    create first validator (part 1)

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -1,2 +1,16 @@
 const Joi = require('joi');
 
+const registerSchema = Joi.object(
+    {
+        username : Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required()
+        .messages({
+            'string.alphanum' :'Username can only cantain letters and numbers.',
+            'string.min' : 'Username must be at least 3 characters.',
+            'any.required' : 'Username is required.'
+        })
+    }
+)

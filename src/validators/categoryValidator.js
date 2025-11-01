8b3ecac03d5fd1ec5c2d2fb1533de4a7dca24acc
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-01 23:54:30 +0330

    starte to define createCategorySchema

--- src/validators/categoryValidator.js
+++ src/validators/categoryValidator.js
@@ -1,1 +1,12 @@
 const Joi = require('joi');
+
+const createCategorySchema = Joi.object({
+    name : Joi.string()
+    .min(3)
+    .max(255)
+    .required()
+    .messages({
+        'string.min' : 'Name must be at least 3 characters long.',
+        'any.required' : 'Name is required.'
+    })
+})

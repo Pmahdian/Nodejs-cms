8939c17f41261ff5dbfa57ef2abcdbd61c1c9254
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-01 22:43:25 +0330

    title of post validation

--- src/validators/postValidator.js
+++ src/validators/postValidator.js
@@ -1,1 +1,12 @@
 const Joi = require('joi');
+
+const creatPostSchema = Joi.object({
+    title : Joi.string()
+    .min(3)
+    .max(255)
+    .required()
+    .message({ 
+        'string.min' : 'Title must be at least 3 characters long.',
+        'any.required' : 'Title is required.'
+    })
+})

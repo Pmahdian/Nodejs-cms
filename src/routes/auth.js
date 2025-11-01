c8ff74eaaac86b4ec53fd31d8e9c9951756de35d
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-01 22:33:07 +0330

    update routes/auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const { register, login } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 const { testAuth } = require('../controllers/testController'); 
+const validateRequest = require('../middleware/validationMiddleware');
+const { registerSchema, loginSchema } = require('../validators/authValidator');
+
 
 // Routes 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateRequest(registerSchema) ,register);
+router.post('/login',validateRequest(loginSchema) ,login);
+
+
 
 
 router.get('/test-protected', authMiddleware, testAuth);
 
-module.exports = router;
+module.exports = router;t

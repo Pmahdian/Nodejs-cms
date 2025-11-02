6d0d7eb8eef888d1eb6250d4f1902c5142476e43
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-02 15:58:34 +0330

    importing

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
 const authMiddleware = require('../middleware/authMiddleware');
+const validatorRequest = require('../middleware/validationMiddleware');
+const {
+    updateProfileSchema 
+} = require('../validators/userValidator');
 
-const {getProfile, updateProfile} = require('../controllers/userController');
+const {
+    getProfile,
+     updateProfile
+    } = require('../controllers/userController');
 
 router.get('/profile', authMiddleware, getProfile);
 router.put('/profile', authMiddleware, updateProfile);

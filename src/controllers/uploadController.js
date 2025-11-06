13f93addc1967eb735d1222693c6350894241495
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-07 00:47:31 +0330

    Where to save it?

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -4,3 +4,9 @@ const path =require('path');
 // =============================================
 // (Storage Configuration)
 // =============================================
+
+
+//Where to save it?
+const storage = multer.diskStorage({
+    destination : function
+})

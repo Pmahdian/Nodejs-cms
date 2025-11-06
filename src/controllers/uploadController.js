b2a57cef26152d5883e051e12a1a9d7e56ad4611
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-07 01:36:29 +0330

    (Storage Configuration)

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,2 +1,11 @@
 const multer = require('multer');
 const path =require('path');
+
+// =============================================
+// (Storage Configuration)
+// =============================================
+const storage = multer.diskStorage({
+    destination : function (req, file, cb) { //cb=callback func
+
+    }
+})

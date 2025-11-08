a726cd12dc7691163276f15ff41c542042ac80c0
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-08 22:05:30 +0330

    POST api

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware')
+const authMiddleware = require('../middleware/authMiddleware');
+const { upload, uploadImage } = require('../controllers/uploadController');
+const {
+    upload,
+    uploadImage
+} = 
+
+//POST /api/upload
+router.post('/',authMiddlewarem, upload.single('image'), uploadImage);
+
+module.exports = router;

d4524959656f5caaaae1c4ab9653245ced81d3d7
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-09 20:34:14 +0330

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 //Router
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static('uploads'));
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/categories', categoriesRoutes);

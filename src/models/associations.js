d4dafff07a70bf0572c9207b53cade5e5e04b8a6
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-10 01:19:10 +0330

    comment belongs to post

--- src/models/associations.js
+++ src/models/associations.js
@@ -13,5 +13,6 @@ Post.belongsTo(Category, {foreignKey : 'category_id'});
 User.hasMany(Comment,{foreignKey : 'user_id'});
 Post.hasMany(Comment,{foreignKey: 'post_id'});
 Comment.belongsTo(User, {foreignKey : 'user_id'});
+Comment.belongsTo(Post,{foreignKey : 'post_id'});
 
 module.exports = { User, Post, Category };

44a1148c5abf64e2315d1564579f0826f0740beb
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-10 01:19:32 +0330

    export Comment

--- src/models/associations.js
+++ src/models/associations.js
@@ -15,4 +15,4 @@ Post.hasMany(Comment,{foreignKey: 'post_id'});
 Comment.belongsTo(User, {foreignKey : 'user_id'});
 Comment.belongsTo(Post,{foreignKey : 'post_id'});
 
-module.exports = { User, Post, Category };
+module.exports = { User, Post, Category, Comment };

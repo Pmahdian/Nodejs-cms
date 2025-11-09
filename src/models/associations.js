42c1656b4361f80375ee8fe966c5e8919096d527
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-10 01:16:29 +0330

    user has many comments

--- src/models/associations.js
+++ src/models/associations.js
@@ -10,4 +10,6 @@ Post.belongsTo(User, {foreignKey : 'user_id'});
 Category.hasMany(Post, {foreignKey : 'category_id'});
 Post.belongsTo(Category, {foreignKey : 'category_id'});
 
+User.hasMany(Comment,{foreignKey : 'user_id'});
+
 module.exports = { User, Post, Category };

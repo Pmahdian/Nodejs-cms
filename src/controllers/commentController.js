f142df515447f906d3464bcf223ef9cdcfb00065
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-10 23:14:06 +0330

    step 1: Receiviing data from differnret places

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,6 +3,11 @@ const { Comment, User, Post} = require('../models/associations');
 // New comment creation function
 const createComment = async (req,res) => {
     try {
+        // step 1: Receiviing data from differnret places
+        const { content } = req.body; 
+        const { id : post_id } = req.params;
+        const user_id = req.user.userId;
+        
         
     } catch (error) {
         

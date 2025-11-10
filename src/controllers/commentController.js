e2134a3ec4592563bd8a69fc4f5b5370f4cdba03
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-10 23:07:47 +0330

    create simple structure of creation comment function

---
+++ src/controllers/commentController.js
@@ -0,0 +1,10 @@
+const { Comment, User, Post} = require('../models/associations');
+
+// New comment creation function
+const createComment = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}

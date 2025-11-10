04abdadce5c2191ea1a1ef79f0435f5dcab22fa3
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-10 23:34:41 +0330

    remove an unuss importion

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require("../config/sequelize");
-const { date } = require('joi');
+
 
 const Comment = sequelize.define('Comment', {
     content : {

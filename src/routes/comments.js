0662cd7e9b9967dc0ca313b44718d515d23160cd
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-11 11:45:59 +0330

    POST api

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router;
-const commmentController = require('../controllers/commentController');
+const { createComment } = require('../controllers/commentController');
 
 const authMiddleware = require('../middleware/authMiddleware');
 const validateRequest = require('../middleware/validationMiddleware');
 
 const { createCommentSchema } = require('../validators/commentValidator');
 
-
+// POST /api/posts/:id/comments
+router.post('/post/:id/comments',
+    authMiddleware,
+    validateRequest(createCommentSchema),
+    createComment
+)

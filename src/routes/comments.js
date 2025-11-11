58983af30d382817cf97178a6b1d8c5a2b05a39a
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-11 11:39:00 +0330

    importing

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const router = express.Router;
+const commmentController = require('../controllers/commentController');
+
+const authMiddleware = require('../middleware/authMiddleware');
+const validateRequest = require('../middleware/validationMiddleware');
+
+const { createCommentSchema } = require('../validators/commentValidator');
+
+

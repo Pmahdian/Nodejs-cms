2984a13feb8c93be964f137355d4bc52e05d5d69
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-15 01:50:53 +0330

    define likes table

--- src/models/Like.js
+++ src/models/Like.js
@@ -1,1 +1,24 @@
 const { DataTypes } = require('sequelize');
+const { sequilize } = require('../config/sequelize');
+
+const Like = sequilize.define('Like', {
+    user_id : {
+       type: DataTypes.INTEGER,
+        allowNull: false 
+    },
+    post_id : {
+        type : DataTypes.INTEGER,
+        allowNull : false
+    },
+    type : {
+        type : DataTypes.ENUM('like', 'bookmark'),
+        allowNull : false
+    }
+},
+{
+    tableName: 'likes',
+    timestamps: true,
+    underscored: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+});

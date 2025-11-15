da58e14d7508a2a6e099778b2aab8e27239e3226
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-16 02:42:32 +0330

    a like belogs to a user

--- src/models/associations.js
+++ src/models/associations.js
@@ -16,7 +16,8 @@ Post.hasMany(Comment,{foreignKey: 'post_id'});
 Comment.belongsTo(User, {foreignKey : 'user_id'});
 Comment.belongsTo(Post,{foreignKey : 'post_id'});
 
-User.hasMany(Like, {foreignKey : user_id});
-Post.hasMany(Like, {foreignKey : post_id});
+User.hasMany(Like, {foreignKey : 'user_id'});
+Post.hasMany(Like, {foreignKey : 'post_id'});
+Like.belongsTo(User, {foreignKey : 'user_id'})
 
 module.exports = { User, Post, Category, Comment };

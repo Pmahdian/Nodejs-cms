398537ca916b6d35d4183b5132f1ed8ea3c2c042
Parnian Mahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-16 02:44:25 +0330

    Update associations (#26)
    * import Datatypes
    
    * define likes table
    
    * export Like
    
    * import Like
    
    * user has many likes
    
    * a Post has many likes
    
    * a like belogs to a user
    
    * a Like belongs to a post
    
    * export Like

--- src/models/associations.js
+++ src/models/associations.js
@@ -2,6 +2,7 @@ const User = require('./User');
 const Post = require('./Post');
 const Category = require('./Category');
 const Comment = require('./Comment')
+const Like = require('./Like');
 
 User.hasMany(Post, {foreignKey : 'user_id'});
 Post.belongsTo(User, {foreignKey : 'user_id'});
@@ -15,4 +16,10 @@ Post.hasMany(Comment,{foreignKey: 'post_id'});
 Comment.belongsTo(User, {foreignKey : 'user_id'});
 Comment.belongsTo(Post,{foreignKey : 'post_id'});
 
-module.exports = { User, Post, Category, Comment };
+User.hasMany(Like, {foreignKey : 'user_id'});
+Post.hasMany(Like, {foreignKey : 'post_id'});
+Like.belongsTo(User, {foreignKey : 'user_id'})
+Like.belongsTo(Post, {foreignKey : 'post_id'})
+
+
+module.exports = { User, Post, Category, Comment, Like };

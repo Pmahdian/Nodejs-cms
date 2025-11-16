fc268bdbedd943b95003f55e6657e371d9f826d3
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-16 12:14:42 +0330

    step 1 : get data

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,1 +1,8 @@
 const { Like, Post, User } = require('../models/associations');
+
+const likePost = async (req, res) => {
+    // step 1 : get data
+    const { id: post_id } = req.params;
+    const user_id = req.user.userId //get userId from req.user moddleware
+    
+}

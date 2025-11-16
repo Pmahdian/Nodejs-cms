306ed35d255a0f89314ed9d3fe029a0a1683b612
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-16 14:51:56 +0330

    all routes need authentication

--- src/routes/likes.js
+++ src/routes/likes.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const {
+    likePost,
+    unlikePost
+} = require('../controllers/likeController');
+const { authenticate } = require('../middleware/authMiddleware');
+
+router.use(authenticate);

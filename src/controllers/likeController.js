b649e739a413b2c3190008efa567b212264890b5
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-16 12:18:12 +0330

    step 2 : checking for existance of the post

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -4,5 +4,14 @@ const likePost = async (req, res) => {
     // step 1 : get data
     const { id: post_id } = req.params;
     const user_id = req.user.userId //get userId from req.user moddleware
-    
+
+
+    // step 2 : checking for existance of the post
+    const post = await Post.findByPk(post_id);
+    if (!post) {
+        res.status(404).json({
+            success : false,
+            message : 'The desired post was not found!'
+        });
+    }
 }

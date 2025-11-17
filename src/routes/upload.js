7086ee291c0fbea113b786dd1084b07bdbcc7d93
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-17 11:33:44 +0330

    debug upload route

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const { upload, uploadImage } = require('../controllers/uploadController');
-const {
-    upload,
-    uploadImage
-} = 
+
 
 //POST /api/upload
 router.post('/',authMiddlewarem, upload.single('image'), uploadImage);
